68a8dc6b4d790a88535e3a3b2d8981fe9a11d1ab
tonomolla6 <tono.iestacio@gmail.com>
2020-09-28 17:34:21 +0200

    Improved bingo project in class

--- BingoJSVanilla/js/main.js
+++ BingoJSVanilla/js/main.js
@@ -1,54 +1,26 @@
-const game = {
-        bombo: [],
-        balls_out: [],
-        start: function () {
-            this.bombo_create();
-        },
-        bombo_create: function () {
-            let bombo = Array(99);
-            for (let i = 0; i < 99; i++) {
-                bombo[i] = i;
-            }
-            this.bombo_mix(bombo);
-        },
-        bombo_mix: function (bombo) {
-            let position = 0;
-            let new_bombo = [];
-
-            for (let g = 0; g < bombo.length; g++) {
-                while (true) {
-                    position = Math.floor(Math.random() * bombo.length);
-                    if (bombo[position] != null) {
-                        new_bombo[g] = bombo[position];
-                        bombo[position] = null;
-                        break;
-                    }
-                }
-            }
-            this.bombo = new_bombo;
-        },
-        catch_ball: function(num) {
-            for (let y = 0; y < num; y++) {
-                while (true) {
-                    var selected = Math.floor(Math.random() * this.bombo.length)
-                    if (Number.isInteger(this.bombo[selected])) {
-                        this.balls_out[this.balls_out.length] = this.bombo[selected];
-                        this.bombo.splice(selected, 1);
-                        this.bombo_mix(this.bombo);
-                        break;
-                    }
-                }
-            }
-        }
+const bombo = {
+    balls: [],
+    balls_out: [],
+    bombo_create: function () {
+        this.balls = Array.from({length: 99}, (_,i) => i + 1);
+        this.shuffle();
+    },
+    shuffle: function () {
+        this.balls.sort((a,b) => Math.random() - 0.5);
+    },
+    catch_ball: function() {
+        let pos = Math.floor(Math.random() * this.balls.length);
+        this.balls_out.push(this.balls.splice(pos, 1)[0]);
     }
+}
 
 document.getElementById('start').addEventListener('click', function() {
-    game.start();
-    console.log(game.bombo);
+    bombo.bombo_create();
+    console.log(bombo.balls);
 }, false);
 
 document.getElementById('get').addEventListener('click', function() {
-    game.catch_ball(1);
-    console.log(game.bombo);
-    console.log(game.balls_out);
+    bombo.catch_ball();
+    console.log(bombo.balls);
+    console.log(bombo.balls_out);
 }, false);
